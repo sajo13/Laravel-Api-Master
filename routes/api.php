4254d1ae1ec0fd08a8c778737048c124c8a5f99c
sajo13 <sajosunny007@gmail.com>
2025-02-18 15:28:13 +0530

    Add Database seed for Ticket and User
    - After seed, Create Api Endpoint for the Tickets Listing.

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,12 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Ticket;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function() {
+    return Ticket::all();
+});
