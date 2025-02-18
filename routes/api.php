f094c79c8c62bb875187c8a3069761073236591e
sajo13 <sajosunny007@gmail.com>
2025-02-18 17:11:03 +0530

    Added Api Version

--- routes/api.php
+++ routes/api.php
@@ -9,33 +9,35 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/register', [AuthController::class, 'register']);
 
-// Route to fetch a all Tickets.
-Route::get('/tickets', function() {
-    return Ticket::all();
-});
+Route::prefix('v1')->group(function() {
+    // Route to fetch a all Tickets.
+    Route::get('/tickets', function() {
+        return Ticket::all();
+    });
 
-// Route to fetch a ticket by its ID
-Route::get('/tickets/{id}', function($id) {
-    return Ticket::findOrFail($id);
-});
+    // Route to fetch a ticket by its ID
+    Route::get('/tickets/{id}', function($id) {
+        return Ticket::findOrFail($id);
+    });
 
 
-// Route to create a new ticket
-Route::post('/tickets', function(Request $request) {
-    $ticket = Ticket::create($request->all());
-    return response()->json($ticket, 201);
-});
+    // Route to create a new ticket
+    Route::post('/tickets', function(Request $request) {
+        $ticket = Ticket::create($request->all());
+        return response()->json($ticket, 201);
+    });
 
-// Update the ticket with the given ID using the request data
-Route::put('/tickets/edit/{id}', function(Request $request, $id) { 
-    $ticket = Ticket::findOrFail($id);
-    $ticket->update($request->all());
-    return response()->json($ticket);
-});
+    // Update the ticket with the given ID using the request data
+    Route::put('/tickets/edit/{id}', function(Request $request, $id) { 
+        $ticket = Ticket::findOrFail($id);
+        $ticket->update($request->all());
+        return response()->json($ticket);
+    });
 
-// Find the ticket by ID and delete it
-Route::delete('/tickets/delete/{id}', function($id) {
-    $ticket = Ticket::findOrFail($id);
-    $ticket->delete();
-    return response()->json(null, 204);
+    // Find the ticket by ID and delete it
+    Route::delete('/tickets/delete/{id}', function($id) {
+        $ticket = Ticket::findOrFail($id);
+        $ticket->delete();
+        return response()->json(null, 204);
+    });
 });
